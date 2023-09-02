6147bfe8b223c334804911502ed1f67886d6013e
RabbitRk <emailtork09@gmail.com>
2023-09-03 01:24:48 +0530

    :sparkles: Authentication and Dashboard added

--- app/page.tsx
+++ app/page.tsx
@@ -1,132 +1,75 @@
 "use client"
 
-import { Button, buttonVariants } from '@/components/ui/button'
 
 import { Metadata } from "next"
-import Link from "next/link"
-
-import { cn } from "@/lib/utils"
 import React from 'react'
-import { Label } from '@/components/ui/label'
-import { Icons } from '@/components/ui/icons'
-import { Input } from '@/components/ui/input'
-// import { buttonVariants } from "@/registry/new-york/ui/button"
-// import { UserAuthForm } from "@/app/examples/authentication/components/user-auth-form"
+import Link from 'next/link';
+import Login from '@/components/login';
+import Image from 'next/image';
+
 
 export const metadata: Metadata = {
   title: "Authentication",
   description: "Authentication forms built using the components.",
 }
 
-
-
-export default function AuthenticationPage() {
-
-  const [isLoading, setIsLoading] = React.useState<boolean>(false)
-
-  async function onSubmit(event: React.SyntheticEvent) {
-    event.preventDefault()
-    setIsLoading(true)
-
-    setTimeout(() => {
-      setIsLoading(false)
-    }, 3000)
-  }
-
+const AuthenticationPage = () => {
   return (
     <>
-
-      <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <Link
-          href="/examples/authentication"
-          className={cn(
-            buttonVariants({ variant: "ghost" }),
-            "absolute right-4 top-4 md:right-8 md:top-8"
-          )}
-        >
-          Login
-        </Link>
-        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-          <div className="flex flex-col space-y-2 text-center">
-            <h1 className="text-2xl font-semibold tracking-tight">
-              Create an account
+      <div className="relative h-full flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+        <div className="relative md:h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+          <div className="absolute inset-0 bg-zinc-900 login-background" />
+          <div className="relative z-20 flex items-center text-lg font-medium">
+            <Image src="/next.svg" width="48" height="48" alt="Logo" />
+           Acme Inc
+          </div>
+          <div className="relative z-20 mt-auto">
+            <h1 className="text-4xl font-semibold tracking-tight">
+              Give your business everything it need to grow
             </h1>
-            <p className="text-sm text-muted-foreground">
-              Enter your email below to create your account
+            <p className="mt-4 text-lg">
+              Give it an extra sales channel with ZERO listing costs.
             </p>
           </div>
-          <div className={cn("grid gap-6")}>
-            <form onSubmit={onSubmit}>
-              <div className="grid gap-2">
-                <div className="grid gap-1">
-                  <Label className="sr-only" htmlFor="email">
-                    Email
-                  </Label>
-                  <Input
-                    id="email"
-                    placeholder="name@example.com"
-                    type="email"
-                    autoCapitalize="none"
-                    autoComplete="email"
-                    autoCorrect="off"
-                    disabled={isLoading}
-                  />
-                  <Input
-                    id="password"
-                    placeholder=""
-                    type="password"
-                    autoCapitalize="none"
-                    autoComplete="password"
-                    autoCorrect="off"
-                    disabled={isLoading}
-                  />
-                </div>
-                <Button disabled={isLoading}>
-                  {isLoading && (
-                    <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-                  )}
-                  Sign In
-                </Button>
-              </div>
-            </form>
-            <div className="relative">
-              <div className="absolute inset-0 flex items-center">
-                <span className="w-full border-t" />
-              </div>
-              <div className="relative flex justify-center text-xs uppercase">
-                <span className="bg-background px-2 text-muted-foreground">
-                  Or continue with
-                </span>
-              </div>
-            </div>
-            <Button variant="outline" type="button" disabled={isLoading}>
-              {isLoading ? (
-                <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-              ) : (
-                <Icons.gitHub className="mr-2 h-4 w-4" />
-              )}{" "}
-              Github
-            </Button>
+          <div className="relative z-20 mt-auto">
+            <blockquote className="space-y-2">
+              <p className="text-lg">Copyright &copy; Acme Inc 2023</p>
+            </blockquote>
           </div>
-          <p className="px-8 text-center text-sm text-muted-foreground">
-            By clicking continue, you agree to our{" "}
-            <Link
-              href="/terms"
-              className="underline underline-offset-4 hover:text-primary"
-            >
-              Terms of Service
-            </Link>{" "}
-            and{" "}
-            <Link
-              href="/privacy"
-              className="underline underline-offset-4 hover:text-primary"
-            >
-              Privacy Policy
-            </Link>
-            .
-          </p>
+        </div>
+        <div className="lg:p-8">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            {/* <div className="flex flex-col space-y-2 text-center">
+              <h1 className="text-2xl font-semibold tracking-tight">
+                Log in as Merchant
+              </h1>
+              <p className="text-sm text-muted-foreground">
+                Enter your email &amp; password to continue.
+              </p>
+            </div> */}
+            <Login />
+            <p className="px-8 text-center text-sm text-muted-foreground pb-6">
+              By logging in, you agree to our <br />
+              <Link
+                href="#"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Terms of Service
+              </Link>{' '}
+              and{' '}
+              <Link
+                href="#"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Privacy Policy
+              </Link>
+              .
+            </p>
+          </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
+
+export default AuthenticationPage;
